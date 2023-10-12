31415f3826e7e0e13fc1e403709aab821b72d81c
Y_Nishino <kaek33586@gmail.com>
2023-10-12 12:42:47 +0900

    add firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,15 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.yakinikudoufu.myapplication"
-    compileSdk = 33
+    namespace = "com.yakinikudoufu.kansouki"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.yakinikudoufu.myapplication"
+        applicationId = "com.yakinikudoufu.kansouki"
         minSdk = 26
         targetSdk = 33
         versionCode = 1
@@ -51,7 +52,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -59,6 +60,10 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.navigation:navigation-compose:2.7.0")
+
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
